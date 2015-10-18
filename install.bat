aa9e45b03f5430a103476a00b3401895ca94c31a
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-10-18 15:03:59 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

---
+++ install.bat
@@ -0,0 +1,57 @@
+@echo off
+
+:BEGIN
+set CYGWIN_ROOT=%~dp0\
+set SETUP_ARCH=setup-x86_64.exe
+set MIRROR=http://mirrors.163.com/cygwin
+
+::Command Line Options:
+::
+:: -D --download                     Download from internet
+:: -L --local-install                Install from local directory
+:: -s --site                         Download site
+:: -O --only-site                    Ignore all sites except for -s
+:: -R --root                         Root installation directory
+:: -x --remove-packages              Specify packages to uninstall
+:: -c --remove-categories            Specify categories to uninstall
+:: -P --packages                     Specify packages to install
+:: -C --categories                   Specify entire categories to install
+:: -p --proxy                        HTTP/FTP proxy (host:port)
+:: -a --arch                         architecture to install (x86_64 or x86)
+:: -q --quiet-mode                   Unattended setup mode
+:: -M --package-manager              Semi-attended chooser-only mode
+:: -B --no-admin                     Do not check for and enforce running as
+::                                   Administrator
+:: -W --wait                         When elevating, wait for elevated child
+::                                   process
+:: -h --help                         print help
+:: -i --ini-basename                 Use a different basename, e.g. "foo",
+::                                   instead of "setup"
+:: -v --verbose                      Verbose output
+:: -l --local-package-dir            Local package directory
+:: -r --no-replaceonreboot           Disable replacing in-use files on next
+::                                   reboot.
+:: -X --no-verify                    Don't verify setup.ini signatures
+:: -I --include-source               Automatically include source download
+:: -n --no-shortcuts                 Disable creation of desktop and start menu
+::                                   shortcuts
+:: -N --no-startmenu                 Disable creation of start menu shortcut
+:: -d --no-desktop                   Disable creation of desktop shortcut
+:: -K --pubkey                       URL of extra public key file (gpg format)
+:: -S --sexpr-pubkey                 Extra public key in s-expr format
+:: -u --untrusted-keys               Use untrusted keys from last-extrakeys
+:: -U --keep-untrusted-keys          Use untrusted keys and retain all
+:: -g --upgrade-also                 also upgrade installed packages
+:: -o --delete-orphans               remove orphaned packages
+:: -Y --prune-install                prune the installation to only the requested
+::                                   packages
+:: -m --mirror-mode                  Skip availability check when installing from
+::                                   local directory (requires local directory to
+::                                   be clean mirror!)
+:: -A --disable-buggy-antivirus      Disable known or suspected buggy anti virus
+::                                   software packages during execution.
+
+
+%CYGWIN_ROOT%\%SETUP_ARCH% -q -g -s "%MIRROR%" -R "%CYGWIN_ROOT%" -P git -P wget -P curl -P vim -P zsh -P shutdown
+
+%CYGWIN_ROOT%\bin\bash --login -i -c "wget --no-check-certificate https://raw.githubusercontent.com/kiddlu/cygbin/master/hello-cygwin -O - | sh"
