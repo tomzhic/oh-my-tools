2a28f4c212645c87ca128fc4facffee5f0eb5d53
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-11-19 15:38:15 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

---
+++ filetype.cmd
@@ -0,0 +1,18 @@
+@echo on
+set CYGWIN_ROOT=%~dp0..
+
+rem bash
+assoc .sh=Shell.File
+ftype Shell.File="%CYGWIN_ROOT%\bin\bash.exe" "%%1" %%*
+reg add "HKEY_CLASSES_ROOT\Shell.File\DefaultIcon" /ve /t REG_SZ /d "%CYGWIN_ROOT%\Cygwin-Terminal.ico,0" /f >nul
+
+rem python
+assoc .py=Python.File
+ftype Python.File="D:\Python\python.exe" "%%1" %%*
+
+rem office
+rem assoc .doc=Office.File
+rem assoc .ppt=Office.File
+rem assoc .=Office.File
+rem assoc .=Office.File
+rem assoc .=Office.File
