c515ee4fbd218b6fe0dd0de949e574aec310fd70
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-11-24 13:23:01 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- terminal.sh
+++ terminal.sh
@@ -7,7 +7,7 @@ ROUTER_IP=192.168.1.1
 echo "connect to $1"
 case "$1" in  
 	ubuntu)
-		startx.cmd &
+		startx.bat &
 		ssh.exe $UBUNTU_IP -l root -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
 	router) ssh.exe $ROUTER_IP -l root -p 22;;
 	*) echo "no target for $1" >&2;;
