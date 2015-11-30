d6c84247d8526cdba82c9f0ff17940478f3dd21b
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-11-30 10:45:43 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- ramparser.sh
+++ ramparser.sh
@@ -11,6 +11,8 @@ then
 	exit 1;
 fi
 
+#strings ./DDRCS0_0.BIN | grep "Linux version" | head -n 1
+
 echo "msm8992 ram parser AP start" 
 rm -rf ap-log
 mkdir ap-log
@@ -21,8 +23,20 @@ python $ramparse_path/ramparse.py \
 	--gdb-path $ndk_path/toolchains/aarch64-linux-android-4.9/prebuilt/windows-x86_64/bin/aarch64-linux-android-gdb \
 	--vmlinux ./vmlinux \
 	-a . \
+	-x --64-bit \
+	--force-hardware 8992 \
+	--vmlinux ./vmlinux \
+	--phys-offset 0x00000000 \
 	--outdir ./ap-log \
-	-x --64-bit --force-hardware 8992
+	--ram-file ./OCIMEM.BIN 0xFE800000 0xFE80FFFF \
+	--ram-file ./DDRCS0_0.BIN 0x00000000 0x2FFFFFFF \
+	--ram-file ./DDRCS0_1.BIN 0x80000000 0xAFFFFFFF \
+	--ram-file ./DDRCS1_0.BIN 0x30000000 0x5FFFFFFF \
+	--ram-file ./DDRCS1_1.BIN 0xB0000000 0xDFFFFFFF
+# 2g	--ram-file ./DDRCS0_0.BIN 0x00000000 0x1FFFFFFF \
+# 2g	--ram-file ./DDRCS0_1.BIN 0x40000000 0x5FFFFFFF \
+# 2g	--ram-file ./DDRCS1_0.BIN 0x20000000 0x3FFFFFFF \
+# 2g	--ram-file ./DDRCS1_1.BIN 0x60000000 0x7FFFFFFF
 
 #tz
 python $ramparse_path/vendor/msm8992/tz_diag_parser.py OCIMEM.BIN DDRCS0_0.BIN > ./ap-log/tz_log.txt
