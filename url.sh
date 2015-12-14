ec5054744a550405d9bf1a7c609539bb6a255064
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-12-14 03:25:42 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- url.sh
+++ url.sh
@@ -7,7 +7,7 @@ if [[ $1 == smb://* ]]; then
 	URL_PATH=`echo ${1#*smb:}`
 	explorer `cygpath -awl $URL_PATH`
 elif [[ $1 == http://* ]]; then
-	open $1
+	explorer $URL_PATH
 else
 	echo "is $1 a url?"
 	exit 1
