52edcbecb35957a23909d1024c931f0c74e1da5d
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-12-14 03:56:28 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- url.sh
+++ url.sh
@@ -5,10 +5,16 @@ URL_PATH=$1
 
 if [[ $1 == smb://* ]]; then
 	URL_PATH=`echo ${1#*smb:}`
-	explorer `cygpath -awl $URL_PATH`
+	URL_PATH=`cygpath -awl $URL_PATH`
+	echo "Server Message Block"
+elif [[ $1 == \\* ]]; then
+	echo "Server Message Block"
 elif [[ $1 == http://* ]]; then
-	explorer $URL_PATH
+	echo "Hyper Text Transfer Protocol"
 else
 	echo "is $1 a url?"
 	exit 1
 fi
+
+echo $URL_PATH
+explorer $URL_PATH
