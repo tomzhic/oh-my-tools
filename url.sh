71bfeebc882b2c0bf2d5902047b5dc40447e4afd
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-12-14 03:13:45 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

---
+++ url.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+source ~/.winixrc
+
+URL_PATH=$1
+
+if [[ $1 == smb://* ]]; then
+	URL_PATH=`echo ${1#*smb:}`
+	explorer `cygpath -awl $URL_PATH`
+elif [[ $1 == http://* ]]; then
+	open $1
+else
+	echo "is $1 a url?"
+	exit 1
+fi
