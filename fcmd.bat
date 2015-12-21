9cecb59b40d38f0756b0ed92fb786ba54d313eb1
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-12-21 03:07:38 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- fcmd.bat
+++ fcmd.bat
@@ -2,7 +2,7 @@
 
 if '%1'=='' start %WINIXROOT%\FreeCommander\FreeCommander.exe & exit /b
 if '%1'=='.' (
-    start %WINIXROOT%\FreeCommander\FreeCommander.exe %CD%
+    start %WINIXROOT%\FreeCommander\FreeCommander.exe /T %CD%
 ) else (
 	start %WINIXROOT%\FreeCommander\FreeCommander.exe %*
 )
