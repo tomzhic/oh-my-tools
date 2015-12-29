f689c1554c8d1652889263a626f96f9a85d03ff6
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-12-29 08:27:09 +0000

    udpate
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- url.sh
+++ url.sh
@@ -11,6 +11,8 @@ elif [[ $1 == \\* ]]; then
 	echo "Server Message Block"
 elif [[ $1 == http://* ]]; then
 	echo "Hyper Text Transfer Protocol"
+elif [[ $1 == https://* ]]; then
+	echo "Hyper Text Transfer Protocol over Secure Socket Layer"
 else
 	echo "is $1 a url?"
 	exit 1
