777859886d3b9bd1f9a2e5466af4dbf24f6794ec
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-01-21 08:30:38 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

---
+++ shell/terminal.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+source ~/.winixrc
+
+UBUNTU_IP=192.168.1.14
+ROUTER_IP=192.168.1.1
+
+echo "connect to $1"
+case "$1" in  
+	ubuntu)
+		startx.bat &
+		ssh.exe $UBUNTU_IP -l root -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
+	router) ssh.exe $ROUTER_IP -l root -p 22;;
+	*) echo "no target for $1" >&2;;
+esac
