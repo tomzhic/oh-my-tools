2f603852ab56cd79df4da3f468629321cd76909c
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-02-18 14:47:18 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- windows/cygwin/gcc.bat
+++ windows/cygwin/gcc.bat
@@ -5,6 +5,6 @@ rem MinGW x64
 set GCC_PREFIX=x86_64-w64-mingw32-
 
 rem MinGW x86
-set GCC_PREFIX=i686-w64-mingw32-
+rem set GCC_PREFIX=i686-w64-mingw32-
 
 %WINIXROOT%\Cygwin\bin\%GCC_PREFIX%gcc.exe %*
