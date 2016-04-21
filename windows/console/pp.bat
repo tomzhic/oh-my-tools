5f9853755a1e3e1398ba1489da324f0cef2b4ccc
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-04-21 11:24:02 +0800

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- windows/console/pp.bat
+++ windows/console/pp.bat
@@ -3,9 +3,10 @@
 rem set TERM_EXE=plink
 set TERM_EXE=putty
 
+for /f "delims=" %%i in (' lscom  ^| busybox grep ProlificSerial ^| busybox awk {"print $2"} ') do (set COM_NUMBER=%%i)
+
 if '%*'=='' call :help & exit /b
 if '%1'=='com' (
-	for /f "delims=" %%i in (' lscom  ^| busybox grep ProlificSerial ^| busybox awk {"print $2"} ') do (set COM_NUMBER=%%i)
 	echo Connect to %COM_NUMBER%
 	start %TERM_EXE% -serial %COM_NUMBER% -sercfg 115200,8,n,1
 ) else if '%1'=='adb' (
