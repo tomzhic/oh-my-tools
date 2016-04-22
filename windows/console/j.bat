89aec9638ec7a25f5e5e4dd962ecdc346062b33f
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-04-22 21:41:12 +0800

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- windows/console/j.bat
+++ windows/console/j.bat
@@ -1,12 +1,22 @@
 @echo off
 
-for /f "delims=" %%i in (' dir  ^| busybox grep "Volume in drive" ^| busybox awk {"print $4"} ') do (set DRIVE_PATH=%%i:)
+set MSG_PATH=%TMP%\kline_msg
 
-if exist "%DRIVE_PATH%\tmp\.path_tag" (
-	for /f "delims=" %%i in ( ' busybox cat %DRIVE_PATH%\tmp\.path_tag ') do (set TAG_PATH=%%i)
+echo %MSG_PATH%
+if exist "%MSG_PATH%" (
+	for /f "delims=" %%i in (' busybox cat %MSG_PATH% ^|  busybox awk {"print $2"} ') do (set CLIENTIP=%%i)
+	busybox echo %CLIENTIP%
+
+	for /f "delims=" %%j in (' net use ^| busybox grep %CLIENTIP% ^| busybox awk {"print $2"} ') do (set DRIVE=%%j)
+	busybox echo %DRIVE%
+
+	for /f "delims=" %%k in (' busybox cat %MSG_PATH% ^|  busybox awk {"print $1"} ') do (set TAG_PATH=%%k)
+	busybox echo %TAG_PATH%
+
+	cd /d %DRIVE%\%TAG_PATH%
+	set OLDPWD=%cd%
+
 ) else (
 	echo No Tag Path
 	exit /b 2
 )
-
-cd %TAG_PATH% 
