dc2c4dc19e24aa3866776a093a0755f0d9a67abf
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-04-25 11:14:44 +0800

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- windows/console/j.bat
+++ windows/console/j.bat
@@ -1,21 +1,24 @@
 @echo off
 
-set MSG_PATH=%TMP%\kline_msg
+set MSG_PATH=%TMP%\ncbus_msg
 
-echo %MSG_PATH%
+::busybox echo %MSG_PATH%
 if not exist "%MSG_PATH%" (
 	echo No Tag Path
 	exit /b 2
 ) 
 
-for /f "delims=" %%i in (' dir ^| busybox cat %MSG_PATH% ^|  busybox awk {"print $2"} ') do (set CLIENTIP=%%i)
-busybox echo %CLIENTIP%
+for /f "delims=" %%i in (' busybox cat %MSG_PATH% ^| busybox grep [JUMP] ^| busybox tail ') do (set MSG_CMD=%%i)
+::busybox echo %MSG_CMD%
 
-for /f "delims=" %%j in (' net use ^| busybox grep %CLIENTIP% ^| busybox awk {"print $2"} ') do (set DRIVE=%%j)
-busybox echo %DRIVE%
+for /f "delims=" %%i in (' busybox echo %MSG_CMD% ^| busybox awk {"print $4"} ') do (set CLIENTIP=%%i)
+::busybox echo %CLIENTIP%
 
-for /f "delims=" %%k in (' dir ^| busybox cat %MSG_PATH% ^|  busybox awk {"print $1"} ') do (set TAG_PATH=%%k)
-busybox echo %TAG_PATH%
+for /f "delims=" %%i in (' net use ^| busybox grep %CLIENTIP% ^| busybox awk {"print $2"} ') do (set DRIVE=%%i)
+::busybox echo %DRIVE%
+
+for /f "delims=" %%i in (' busybox echo %MSG_CMD% ^| busybox awk {"print $3"} ') do (set TAG_PATH=%%i)
+::busybox echo %TAG_PATH%
 
 cd /d %DRIVE%\%TAG_PATH%
 set OLDPWD=%cd%
