11dffac3bffa89ac2e5cc2c0eb5249bcb1cebc86
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-05-23 16:23:37 +0800

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- windows/console/ncbd.bat
+++ windows/console/ncbd.bat
@@ -3,33 +3,32 @@
 set MSG_PATH=%TMP%\ncbus_msg
 set PORT_NUM=8963
 
-if '%*'=='' call :help & exit /b
+::if '%1'=='-h' call :help & exit /b
 
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
 
 for /f "delims=" %%i in (' netstat -aon ^| busybox grep 8963 ^| busybox awk {"print $5"} ') do (set PID=%%i)
 
-if '%1'=='msg' (
-	busybox cat %MSG_PATH%
-) else if '%1'=='clear' (
-	busybox echo > %MSG_PATH%
-)  else if '%1'=='start' (
+if '%*'=='' (
 	busybox echo >> %MSG_PATH%
 	busybox echo ===============%ldt%=============== >> %MSG_PATH%
 	start /b nc.exe -L -p %PORT_NUM% >> %MSG_PATH%"
 ) else if '%1'=='stop' (
-	::busybox echo kill %PID%
 	nircmd killprocess /%PID%
+) else if '%1'=='msg' (
+	busybox cat %MSG_PATH%
+) else if '%1'=='clear' (
+	busybox echo > %MSG_PATH%
 ) else (
-	echo do not regonize
+	call :help & exit /b
 )
 exit /b
 
 :help
-echo This is the unix like cd command which used in windows cmd
+echo This is my simple netcat bus(ncb) deamon for personal use
 echo.
-echo ncb msg   : show msg log
-echo ncb clear : clear msg log
-echo ncb start : start service
-echo ncb start : stop service
+echo ncbd       : start service
+echo ncbd stop  : stop service
+echo ncbd msg   : show msg log
+echo ncbd clear : clear msg log
--- windows/wrapper/emacs.bat
+++ windows/wrapper/emacs.bat
@@ -4,4 +4,8 @@
 ::%WINIXROOT%\Cygwin\bin\bash.exe -i -c "/bin/emacs-w32.exe %*"
 
 ::windows version
-%WINIXROOT%\Emacs\bin\emacs.exe %*
+if not '%1'=='--help' (
+	start "" %WINIXROOT%\Emacs\bin\emacs.exe %*
+) else (
+	%WINIXROOT%\Emacs\bin\emacs.exe --help
+)
